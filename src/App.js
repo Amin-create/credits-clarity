eaacd4cc0e673e74a6e23743258bb91c394d1a54
Amin-create <amin@octalyte.com>
2023-10-30 16:59:08 +0500

    start(initialization and header): project inatialized and header component created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Header from './components/core/Header.js';
+
+function App() {
+  return (
+    <main className="">
+      <Header />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/core/Header.js
@@ -0,0 +1,59 @@
+import React from 'react'
+// import logo from "../../assets/images/logo/logo.png"
+import logo2 from "../../assets/images/logo/logo.svg"
+
+function Header() {
+  return (
+    <div>
+      <nav class="border-b-[6px] border-[#09EC92] bg-white dark:bg-gray-800 shado w py-7 ">
+        <div class="px-8 mx-auto max-w-[1170px]">
+          <div class="flex items-center justify-between h-16">
+            <div class=" flex items-center">
+              <a class="flex-shrink-0" href="/">
+                <img class="w-[130px]" src={logo2} alt="Workflow" />
+              </a>
+              <div class="hidden md:block">
+                <div class="flex items-baseline ml-10 space-x-3">
+                  <a class="text-[16px] text-[#09EC92] font-bold py-2" href="/#">
+                    Home
+                  </a>
+                  <a class="text-[16px] hover:text-[#09EC92] font-normal px-3 py-2" href="/#">
+                    About Us
+                  </a>
+                  <a class="text-[16px] hover:text-[#09EC92] font-normal px-3 py-2" href="/#">
+                    Features
+                  </a>
+                  <a class="text-[16px] hover:text-[#09EC92] font-normal px-3 py-2" href="/#">
+                    Contact Us
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="block">
+              <div class="flex items-center ml-4 md:ml-6">
+                <button type="button" class="w-full text-[16px] font-medium whitespace-nowrap rounded-md px-4 py-2 ">
+                  Sign in
+                </button>
+                <button type="button" class="w-full text-[#fff] text-[16px] font-medium whitespace-nowrap rounded-full bg-[#09EC92] px-6 py-3  ">
+                  Sign up free →
+                </button>
+              </div>
+            </div>
+            <div class="flex -mr-2 md:hidden">
+              <button class="text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
+                <svg width="20" height="20" fill="currentColor" class="w-8 h-8" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z">
+                  </path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+
+      </nav>
+    </div>
+
+  )
+}
+
+export default Header
