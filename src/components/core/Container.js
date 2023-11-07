245446e56ba93a3d57d44a8439b5ff0caedb23fd
Amin-create <amin@octalyte.com>
2023-11-07 22:12:15 +0500

    style(responsiveness): improvement

--- src/components/core/Container.js
+++ src/components/core/Container.js
@@ -4,7 +4,7 @@ function Container({ children, wider }) {
     return (
         <>
             {/* <div className={`mx-auto ${wider ? "max-w-[1460px]" : "max-w-[1170px]"}`}> */}
-            <div className={`mx-auto sm:max-w-[640px] md:max-w-[768px] lg:max-w-[976px] xl:max-w-[1170px] ${wider ? "max-w-[1460px]" : "max-w-[1170px]"} px-10 md:px-0`}>
+            <div className={`mx-auto sm:max-w-[640px] md:max-w-[768px] lg:max-w-[976px] xl:max-w-[1170px] ${wider ? "max-w-[1460px]" : "max-w-[1170px]"} px-6 sm:px-10 md:px-0`}>
                 {children}
             </div>
         </>
--- src/components/home/faq/Faq.js
+++ src/components/home/faq/Faq.js
@@ -18,22 +18,21 @@ const FAQ = ({ data }) => {
 
     const handleToggle = (index) => {
         if (openIndex !== index) {
-            // Clicked on a closed FAQ, so open it.
             setOpenIndex(index);
         }
     };
 
     return (
-        <div className='pt-16 md:pt-24 pb-20 md:pb-28'>
+        <div className='pt-8 sm:pt-14 md:pt-24 pb-20 md:pb-28'>
             <div className="flex justify-between items-center gap-x-10 gap-y-5">
-                <h2 className="w-[50%] text-[48px] leading-[56px] font-extrabold">
+                <h2 className="w-[50%] text-[28px] sm:text-[48px] leading-[34px] sm:leading-[56px] font-extrabold">
                     FAQs.
                 </h2>
                 <h2 className="hidden lg:block w-[50%] text-[48px] leading-[56px] font-extrabold">
                     Ans.
                 </h2>
             </div>
-            <div className="flex flex-col lg:flex-row justify-between gap-5 md:gap-10 mt-8">
+            <div className="flex flex-col lg:flex-row justify-between gap-5 md:gap-10 mt-4 sm:mt-8">
                 <div className="flex flex-col justify-start items-start gap-x-4 gap-y-2 md:gap-y-4">
                     {data?.map((item, index) => (
                         <div key={index} className="flex justify-between items-center w-full gap-x-5 md:gap-x-10">
@@ -41,13 +40,13 @@ const FAQ = ({ data }) => {
                         </div>
                     ))}
                 </div>
-                <div data-aos="fade-up" className="w-full h-[auto] md:h-[615px] rounded-[16px] bg-[#09EC92] p-6 md:p-10">
+                <div data-aos="fade-up" className="w-full h-[auto] md:h-[615px] rounded-[16px] bg-[#09EC92] p-4 sm:p-6 md:p-10">
                     {openIndex !== null && (
                         <>
                             <span className='text-dark-blue text-[20px] md:text-[30px]'>
                                 <TiStarburst />
                             </span>
-                            <p className="text-gray-700 text-[18px] md:text-[24px] leading-[28px] md:leading-[36px] font-regular mt-5 transition-all">
+                            <p className="text-gray-700 text-[14px] sm:text-[18px] md:text-[24px] leading-[22px] sm:leading-[28px] md:leading-[36px] font-regular mt-2 sm:marker:mt-5 transition-all">
                                 {data[openIndex].answer}
                             </p>
                         </>
