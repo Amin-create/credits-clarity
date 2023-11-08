9382b072a644e58b966c861022cdd288c65df5d9
Amin-create <amin@octalyte.com>
2023-11-08 22:25:11 +0500

    QA(hero section adjustment): done

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -4,10 +4,10 @@ import { Core } from '../../components';
 function Hero({ data }) {
     return (
         <section className='w-full relative overflow-hidden'>
-            <div className='absolute z-[-1] w-full h-[330px] sm:h-[340px] md:h-[572px] rounded-bl-[50px] sm:rounded-bl-[90px] md:rounded-bl-[300px] bg-gradient-to-r from-gr-a-1 to-gr-a-2'>
+            <div className='absolute z-[-1] w-full h-[310px] sm:h-[340px] md:h-[472px] 2xl:h-[572px] rounded-bl-[50px] sm:rounded-bl-[90px] md:rounded-bl-[140px] 2xl:rounded-bl-[300px] bg-gradient-to-r from-gr-a-1 to-gr-a-2'>
             </div>
             <Core.Container>
-                <div className="flex justify-between items-start h-[350px] sm:h-[390px] md:h-[620px] 2xl:h-[720px] pt-[30px] sm:pt-[40px] xl:pt-[80px] 2xl:pt-[130px]">
+                <div className="flex justify-between items-start h-[340px] sm:h-[390px] md:h-[620px] 2xl:h-[720px] pt-[30px] sm:pt-[40px] xl:pt-[80px] 2xl:pt-[130px]">
                     <div>
                         <h1 className="max-w-full md:max-w-[620px] 2xl:max-w-[540px] text-[#fff] text-[30px] sm:text-[40px] md:text-[52px] leading-[40px] sm:leading-[50px] md:leading-[60px]  2xl:leading-[70px] font-bold ">
                             {data?.heading}
@@ -18,7 +18,7 @@ function Hero({ data }) {
                         <Core.Button greenIconicLg to="/signup">Sign up free</Core.Button>
                     </div>
                     <div data-aos="zoom-in" className='mr-[-120px] 2xl:mr-[-200px] hidden lg:block'>
-                        <img className="scale-[0.75] 2xl:scale-[1] mt-[-100px] 2xl:mt-[-60px]" src={data?.image} alt="logo" />
+                        <img className="scale-[0.75] md:scale-[0.8] 2xl:scale-[1] mt-[-100px] md:mt-[-50px] 2xl:mt-[-60px]" src={data?.image} alt="logo" />
                     </div>
                 </div>
             </Core.Container>
