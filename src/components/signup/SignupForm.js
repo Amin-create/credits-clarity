051401649502aec1cd891121fd112f8c7789fd82
Amin-create <amin@octalyte.com>
2023-11-08 16:30:46 +0500

    style(signin and signup): update

--- src/components/signup/SignupForm.js
+++ src/components/signup/SignupForm.js
@@ -24,9 +24,9 @@ function SignupForm() {
         <section className='w-full relative'>
             <Core.Container wider>
                 <div className="flex justify-center items-center pt-14 md:pt-28 pb-20 md:pb-72">
-                    <div className='w-full max-w-[700px] rounded-[30px] bg-white px-6 md:px-24 pt-8 md:pt-14 pb-6 md:pb-12'>
+                    <div className='w-full max-w-[700px] rounded-[30px] bg-white px-6 md:px-24 pt-6 sm:pt-8 md:pt-14 pb-6 md:pb-12'>
                         <form>
-                            <h1 className='text-dark-blue text-[40px] md:text-[52px] leading-[50px] md:leading-[70px] tracking-[-1px] font-bold text-center mx-auto mb-[20px] md:mb-[60px]'>
+                            <h1 className='text-dark-blue text-[28px] sm:text-[40px] md:text-[52px] leading-[35px] sm:leading-[50px] md:leading-[70px] tracking-[-1px] font-bold text-center mx-auto mb-[10px] sm:mb-[20px] md:mb-[60px]'>
                                 Sign up
                             </h1>
                             <div className="grid grid-cols-1 gap-x-4 md:gap-x-16 gap-y-4 md:gap-y-10 sm:grid-cols-6 pb-6">
